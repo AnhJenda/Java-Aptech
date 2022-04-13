3016b89213b581b9f70e114d3cc7ebd2b0847bf1
AnhJenda <93340795+AnhJenda@users.noreply.github.com>
2022-04-13 20:56:36 +0700

    update

--- CodeleanInheritance/src/Person/TestPerson.java
+++ CodeleanInheritance/src/Person/TestPerson.java
@@ -7,34 +7,8 @@ package Person;
 
 public class TestPerson {
     public static void main(String[] args) {
-        /* Test Student class */
-        Student s1 = new Student("Tan Ah Teck", "1 Happy Ave");
-        s1.addCourseGrade("IM101", 97);
-        s1.addCourseGrade("IM102", 68);
-        s1.printGrades();
+        Person p1 = new Person("Quang Anh", "Ha Noi");
 
-        System.out.println("Average is " + s1.getAverageGrade());
-        //Average is 82.5
-
-        /* Test Teacher class */
-        Teacher t1 = new Teacher("Paul Tan", "8 sunset way");
-        System.out.println(t1);
-
-        String[] courses = {"IM101", "IM102", "IM101"};
-        for (String course: courses) {
-            if (t1.addCourse(course)) {
-                System.out.println(course + " added");
-            } else {
-                System.out.println(course + " cannot be added");
-            }
-        }
-
-        for (String course: courses) {
-            if (t1.removeCourse(course)) {
-                System.out.println(course + " removed");
-            } else {
-                System.out.println(course + " cannot be removed");
-            }
-        }
+        System.out.println(p1);
     }
 }
